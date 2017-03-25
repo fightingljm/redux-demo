3f8c310c21b53edc1d7ced0a9c402ecfeec6fa6e
liujinmeng <1321839748@qq.com>
2017-03-25 20:57:56 +0800

    react-redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
+
 import PostBody from './components/PostBody';
 import CommentBox from './components/CommentBox';
+
+import {Provider} from 'react-redux';
+import store from './store'
 
 class App extends React.Component {
   render () {
     return(
-      <div>
-        app
-        <div className="top  clearfix">
-          <PostBody/>
+      <Provider store={store}>
+        <div>
+          <div className="top  clearfix">
+            <PostBody/>
+          </div>
+          <div className="bottom  clearfix">
+            <CommentBox/>
+          </div>
         </div>
-        <div className="bottom  clearfix">
-          <CommentBox/>
-        </div>
-      </div>
+      </Provider>
     )
   }
 }
--- src/components/PostBody.js
+++ src/components/PostBody.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import store from '../store';
+import {connect} from 'react-redux';
 
 class PostBody extends React.Component {
-  constructor() {
-    super();
-    this.state={
-      num:store.getState().length
-    }
-  }
   render(){
     return(
       <div className="post-body">
         PostBody
         <div className="comment-num">
-          {this.state.num} 评论
+          {this.props.comments.length} 评论
         </div>
       </div>
     )
   }
 }
 
-export default PostBody;
+const mapStateToProps = (state) => ({
+  comments:state
+})
+
+export default connect(mapStateToProps)(PostBody);
