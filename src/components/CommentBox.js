e93fcd6508e51778494f30fca850f6fa95632de8
liujinmeng <1321839748@qq.com>
2017-03-25 19:57:14 +0800

    commentList

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -4,13 +4,22 @@ class CommentBox extends React.Component {
   constructor() {
     super();
     this.state={
-
+      comments:[
+        "hello1",
+        "hello2"
+      ]
     }
   }
   render(){
+    let  commentList = this.state.comments.map(item => {
+      return(
+        <div key={Math.random()} className="comment">{item}</div>
+      )
+    })
     return(
       <div className="comment-box">
         CommentBox
+        {commentList}
       </div>
     )
   }
