123f1c1b958d0e8c8505b2c8d4e8d54dee977f39
liujinmeng <1321839748@qq.com>
2017-03-27 16:57:38 +0800

    commentbox-mapStateToProps

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import store from '../store';
+import {connect} from 'react-redux'
 
 class CommentBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state={
-      comments:store.getState().comments[this.props.id]
-    }
-  }
   handleSubmit(e){
     e.preventDefault();
-    console.log(store.getState());
     store.dispatch({type:'ADD_COMMENT',comment:this.refs.content.value,postId:this.props.id});
-    console.log(store.getState());
     this.refs.commentForm.reset();
-    this.setState({comments:store.getState().comments[this.props.id]})
   }
   render(){
     console.log(this.props.id);
-    let  commentList = this.state.comments.map(item => {
+    let  commentList = this.props.comments[this.props.id].map(item => {
       return(
         <div key={Math.random()} className="comment">{item}</div>
       )
@@ -36,5 +28,7 @@ class CommentBox extends React.Component {
     )
   }
 }
-
-export default CommentBox;
+const mapStateToProps = (state) => ({
+  comments:state.comments
+})
+export default connect(mapStateToProps)(CommentBox);
