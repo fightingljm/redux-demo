9542c253e6bbcb09de10c8eb4d4bbfc833dc42de
liujinmeng <1321839748@qq.com>
2017-03-27 11:56:15 +0800

    INCREMENT_LIKES

--- src/components/PostBody.js
+++ src/components/PostBody.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import store from '../store'
 
 class PostBody extends React.Component {
+  handleClick(){
+    store.dispatch({type:'INCREMENT_LIKES',index:this.props.id -1})
+  }
   render(){
     console.log(this.props.posts[this.props.id -1 ].likes);
     return(
       <div className="post-body">
         PostBody
-        <div className="love-num num">
+        <div className="love-num num" onClick={this.handleClick.bind(this)}>
           {this.props.posts[this.props.id -1 ].likes} 喜欢
         </div>
         <div className="comment-num num">
